10ba3f2f523f65cb23c3023e5d777f3e6c811edd
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-10-17 19:26:24 -0600

    copy business logic to package files

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,1 +1,30 @@
 package config
+
+import (
+  "github.com/spf13/viper"
+)
+
+type Config struct {
+	ApiKey 		string `mapstructure:"api_key"`
+	HomeCity 	string `mapstructure:"home_city"`
+}
+
+func config() (Config, error) {
+	vp := viper.New()
+	var config Config
+
+	vp.SetConfigName("config")
+	vp.SetConfigType("json")
+	vp.AddConfigPath(".")
+	err := vp.ReadInConfig()
+	if err != nil {
+		return Config{}, err
+	}
+
+	err = vp.Unmarshal(&config)
+	if err != nil {
+		return Config{}, err
+	}
+
+	return config, nil
+}
--- pkg/geocoding/geocoding.go
+++ pkg/geocoding/geocoding.go
@@ -1,1 +1,41 @@
 package geocoding
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+type Geocoding struct {
+	City      string  `json:"name"`
+	State     string  `json:"state"`
+	Country   string  `json:"country"`
+	Latitude  float64 `json:"lat"`
+	Longitude float64 `json:"lon"`
+}
+
+func CityGeo(city string, key string) Geocoding {
+	endpoint := fmt.Sprintf("http://api.openweathermap.org/geo/1.0/direct?q=%s&limit=1&appid=%s", city, key)
+	res, err := http.Get(endpoint)
+	if err != nil {
+	  log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		log.Fatalf("Geocoding API status: %d\n", res.StatusCode)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var geocoding []Geocoding
+	err = json.Unmarshal(body, &geocoding)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return geocoding[0]
+}
